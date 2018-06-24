81dcd3b49338e04623ddca8969b7b0e486cc8880
JuKu <kuenzel.justin@t-online.de>
2018-06-24 20:30:12 +0200

    added main class.

--- desktop/src/main/java/com/jukusoft/rts/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/jukusoft/rts/desktop/DesktopLauncher.java
@@ -1,9 +1,26 @@
 package com.jukusoft.rts.desktop;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import java.io.IOException;
+
 public class DesktopLauncher {
 
-    public static void main (String[] args) {
-        //
+    public static void main (String[] args) throws IOException {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+        //load window config
+        WindowConfig windowConfig = new WindowConfig("./config/window.cfg");
+        windowConfig.fillConfig(config);
+
+        // start game
+        new Lwjgl3Application(new ApplicationAdapter() {
+            //
+        }, config);
+
+        System.exit(0);
     }
 
 }
