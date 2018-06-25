c185c2ae5d32013144555f886cf59cef04190ec5
JuKu <kuenzel.justin@t-online.de>
2018-06-25 23:57:25 +0200

    fixed some problems in extract.sh script.

--- extract.sh
+++ extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Making msgs/messages.pot from all java sources"
+echo "Making data/i18n/messages.pot from all java sources"
 find . -name '*.java' > .files-to-extract
 xgettext -L Java -i --no-wrap --from-code utf-8 -F -f .files-to-extract \
          -ktrc:1c,2 -ktrcw:1c,2 -ktrcf:1c,2 -ktrcfw:1c,2 -ktr -ktrj -ktrfj \
