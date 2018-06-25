7312b84c2c24ef0340010f6f6b7aeffbb31a5499
JuKu <kuenzel.justin@t-online.de>
2018-06-25 20:16:17 +0200

    fixed some problems.

--- desktop/src/main/java/com/jukusoft/rts/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/jukusoft/rts/desktop/DesktopLauncher.java
@@ -3,6 +3,7 @@ package com.jukusoft.rts.desktop;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.jukusoft.rts.core.Game;
+import com.jukusoft.rts.core.version.Version;
 import com.jukusoft.rts.game.RTSGame;
 import com.jukusoft.rts.gui.GameGUI;
 
@@ -11,6 +12,9 @@ import java.io.IOException;
 public class DesktopLauncher {
 
     public static void main (String[] args) throws IOException {
+        //set version information
+        Version.setInstance(new Version(DesktopLauncher.class));
+
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
         //load window config
