d17f2cf3e038fe8eca88a0e262bf15f8538af3a1
JuKu <kuenzel.justin@t-online.de>
2018-06-25 23:21:58 +0200

    changed travis ci configuration, added support for i18n.

--- extract.sh
+++ extract.sh
@@ -3,5 +3,5 @@ echo "Making msgs/messages.pot from all java sources"
 find . -name '*.java' > .files-to-extract
 xgettext -L Java -i --no-wrap --from-code utf-8 -F -f .files-to-extract \
          -ktrc:1c,2 -ktrcw:1c,2 -ktrcf:1c,2 -ktrcfw:1c,2 -ktr -ktrj -ktrfj \
-         -ktrw -ktrf -ktrfw --force-po --omit-header -o msgs/messages.pot
+         -ktrw -ktrf -ktrfw --force-po --omit-header -o data/po/messages.pot
 rm .files-to-extract
