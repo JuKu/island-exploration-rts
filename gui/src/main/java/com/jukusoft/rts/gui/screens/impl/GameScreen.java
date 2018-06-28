9fd5413ab2e2e915b9850f1d1a612c2c408e4d35
JuKu <kuenzel.justin@t-online.de>
2018-06-29 00:05:02 +0200

    added camera, closed #16 .

--- gui/src/main/java/com/jukusoft/rts/gui/screens/impl/GameScreen.java
+++ gui/src/main/java/com/jukusoft/rts/gui/screens/impl/GameScreen.java
@@ -1,10 +1,24 @@
 package com.jukusoft.rts.gui.screens.impl;
 
 import com.jukusoft.rts.core.Game;
+import com.jukusoft.rts.core.logging.LocalLogger;
+import com.jukusoft.rts.core.time.GameTime;
+import com.jukusoft.rts.gui.camera.CameraHelper;
 import com.jukusoft.rts.gui.screens.IScreen;
 import com.jukusoft.rts.gui.screens.ScreenManager;
+import com.teamunify.i18n.I;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GameScreen implements IScreen {
+
+    //camera
+    protected CameraHelper camera = null;
+
+    //game time (for simulation)
+    protected GameTime time = GameTime.getInstance();
+
+    protected AtomicBoolean loaded = new AtomicBoolean(false);
 
     @Override
     public void onStart(Game game, ScreenManager<IScreen> screenManager) {
@@ -18,17 +32,20 @@ public class GameScreen implements IScreen {
 
     @Override
     public void onResume(Game game) {
-        //
+        if (!loaded.get()) {
+            throw new IllegalStateException("game screen wasnt loaded yet, call loadAsync() first.");
+        }
     }
 
     @Override
     public void onPause(Game game) {
-        //
+        this.camera = null;
     }
 
     @Override
     public void onResize(int width, int height) {
-        //
+        //resize camera
+        this.camera.resize(width, height);
     }
 
     @Override
@@ -38,7 +55,8 @@ public class GameScreen implements IScreen {
 
     @Override
     public void update(Game game, ScreenManager<IScreen> screenManager) {
-        //
+        //update main camera
+        this.camera.update(time);
     }
 
     @Override
@@ -50,7 +68,15 @@ public class GameScreen implements IScreen {
     * will be executed from LoadGameScreen in another thread
     */
     public void loadAsync () {
+        LocalLogger.print(I.tr("create camera"));
+
+        //create new camera
+        this.camera = new CameraHelper(1280, 720);
+
         //load resources asynchronous
+
+        //set flag
+        this.loaded.set(true);
     }
 
 }
