10733c654c9aa5c34ac94124dcf82add380d1e77
JuKu <kuenzel.justin@t-online.de>
2018-06-29 21:59:29 +0200

    added some junit tests.

--- game/src/main/java/com/jukusoft/rts/game/RTSGame.java
+++ game/src/main/java/com/jukusoft/rts/game/RTSGame.java
@@ -56,6 +56,9 @@ public class RTSGame implements Game {
 
         //set flag
         this.newGame = false;
+
+        //TODO: remove this line later
+        this.map = new MapMeta();
     }
 
     @Override
