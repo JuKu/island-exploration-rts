ae8bf1bdb8b38e4dfc56dc6b24ed00fdc4476342
JuKu <kuenzel.justin@t-online.de>
2018-07-01 18:41:17 +0200

    marked class TsxTileset as deprecated.

--- core/src/main/java/com/jukusoft/rts/core/tiled/tileset/TsxTileset.java
+++ core/src/main/java/com/jukusoft/rts/core/tiled/tileset/TsxTileset.java
@@ -2,6 +2,16 @@ package com.jukusoft.rts.core.tiled.tileset;
 
 import java.io.File;
 
+/**
+* external tileset file
+ *
+ * @deprecated since 0.0.1 because it is converted to TextureTileset automatically
+ *
+ * @see TextureTileset
+ * @see TsxParser
+ * @see com.jukusoft.rts.core.tiled.TiledMapParser
+*/
+@Deprecated
 public class TsxTileset extends Tileset {
 
     public TsxTileset(int firstTileID, String source, int tileCount) {
