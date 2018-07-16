e16d37c859cd3699714dd26059d21a2dcb13d7d3
JuKu <kuenzel.justin@t-online.de>
2018-07-16 19:26:36 +0200

    added FPS label, closed #34 .

--- gui/src/main/java/com/jukusoft/rts/gui/screens/impl/HUDScreen.java
+++ gui/src/main/java/com/jukusoft/rts/gui/screens/impl/HUDScreen.java
@@ -1,13 +1,19 @@
 package com.jukusoft.rts.gui.screens.impl;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.jukusoft.rts.core.Game;
 import com.jukusoft.rts.gui.assetmanager.GameAssetManager;
+import com.jukusoft.rts.gui.fps.FPSManager;
 import com.jukusoft.rts.gui.input.InputManager;
 import com.jukusoft.rts.gui.screens.IScreen;
 import com.jukusoft.rts.gui.screens.ScreenManager;
+import com.jukusoft.rts.gui.utils.SkinFactory;
 
 public class HUDScreen implements IScreen {
 
@@ -17,14 +23,20 @@ public class HUDScreen implements IScreen {
     //stage which contains widgets
     protected Stage stage = null;
 
+    protected Pixmap labelColor = null;
+
     //global asset manager
     protected GameAssetManager assetManager = GameAssetManager.getInstance();
 
     //flag, if HUD should be drawn
     protected boolean show = true;
 
+    //skins
+    protected Skin skin2 = null;
+
     //widgets
     protected Image logo = null;
+    protected Label fpsLabel = null;
 
     public HUDScreen () {
         //
@@ -37,6 +49,9 @@ public class HUDScreen implements IScreen {
 
         //load assets
         this.assetManager.load(LOGO_PATH, Texture.class);
+
+        //load skins
+        this.skin2 = SkinFactory.createSkin("./data/misc/skins/libgdx/uiskin.json");
     }
 
     @Override
@@ -49,10 +64,20 @@ public class HUDScreen implements IScreen {
         //add input processor
         InputManager.getInstance().addFirst(this.stage);
 
+        //add logo
         assetManager.finishLoading(LOGO_PATH);
         Texture logoTexture = assetManager.get(LOGO_PATH, Texture.class);
         this.logo = new Image(logoTexture);
         stage.addActor(this.logo);
+
+        this.fpsLabel = new Label("FPS: ????", this.skin2);
+
+        //set label background color
+        labelColor = new Pixmap((int) this.fpsLabel.getWidth(), (int) this.fpsLabel.getHeight(), Pixmap.Format.RGBA8888);
+        labelColor.setColor(Color.valueOf("#001f3f"));//#36581a, #0074D9, #001f3f
+        labelColor.fill();
+        this.fpsLabel.getStyle().background = new Image(new Texture(labelColor)).getDrawable();
+        stage.addActor(fpsLabel);
     }
 
     @Override
@@ -68,6 +93,9 @@ public class HUDScreen implements IScreen {
 
         logo.setX((stage.getWidth() - logo.getWidth()) / 2);
         logo.setY(stage.getHeight() - logo.getHeight());
+
+        fpsLabel.setX(20);
+        fpsLabel.setY(20);
     }
 
     @Override
@@ -77,7 +105,8 @@ public class HUDScreen implements IScreen {
 
     @Override
     public void update(Game game, ScreenManager<IScreen> screenManager) {
-        //we dont need to do anything here
+        //update FPS
+        fpsLabel.setText("FPS: " + FPSManager.getInstance().getFPS());
     }
 
     @Override
