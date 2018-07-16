f6af7fb725223f189e7bad39fe2bc7942cae773a
JuKu <kuenzel.justin@t-online.de>
2018-07-16 18:44:01 +0200

    added support to draw a HUD screen, closed #36 .

--- gui/src/main/java/com/jukusoft/rts/gui/screens/impl/HUDScreen.java
+++ gui/src/main/java/com/jukusoft/rts/gui/screens/impl/HUDScreen.java
@@ -1,6 +1,8 @@
 package com.jukusoft.rts.gui.screens.impl;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.jukusoft.rts.core.Game;
 import com.jukusoft.rts.gui.assetmanager.GameAssetManager;
 import com.jukusoft.rts.gui.input.InputManager;
@@ -8,6 +10,9 @@ import com.jukusoft.rts.gui.screens.IScreen;
 import com.jukusoft.rts.gui.screens.ScreenManager;
 
 public class HUDScreen implements IScreen {
+
+    //paths
+    protected static final String LOGO_PATH = "./data/misc/logo/logo.png";
 
     //stage which contains widgets
     protected Stage stage = null;
@@ -18,6 +23,9 @@ public class HUDScreen implements IScreen {
     //flag, if HUD should be drawn
     protected boolean show = true;
 
+    //widgets
+    protected Image logo = null;
+
     public HUDScreen () {
         //
     }
@@ -26,17 +34,25 @@ public class HUDScreen implements IScreen {
     public void onStart(Game game, ScreenManager<IScreen> screenManager) {
         //create new stage
         this.stage = new Stage();
+
+        //load assets
+        this.assetManager.load(LOGO_PATH, Texture.class);
     }
 
     @Override
     public void onStop(Game game) {
-
+        this.assetManager.unload(LOGO_PATH);
     }
 
     @Override
     public void onResume(Game game) {
         //add input processor
         InputManager.getInstance().addFirst(this.stage);
+
+        assetManager.finishLoading(LOGO_PATH);
+        Texture logoTexture = assetManager.get(LOGO_PATH, Texture.class);
+        this.logo = new Image(logoTexture);
+        stage.addActor(this.logo);
     }
 
     @Override
@@ -52,6 +68,9 @@ public class HUDScreen implements IScreen {
         stage.getViewport().update(width, height, true);*/
         stage.getViewport().setScreenPosition(0, 0);
         stage.getViewport().setScreenSize(width, height);
+
+        logo.setX((stage.getWidth() - logo.getWidth()) / 2);
+        logo.setY(stage.getHeight() - logo.getHeight());
     }
 
     @Override
